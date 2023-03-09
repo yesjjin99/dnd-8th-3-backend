3e5f732482d4bf2696e34004beb4c013d6db4d2f
minseokkang <aaaa727@khu.ac.kr>
2023-03-10 00:47:56 +0900

    [D83T-73] 프로필 업데이트 API - 프로필 이미지 수정 관련 코드 추가

--- src/main/java/d83t/bpmbackend/domain/aggregate/profile/service/ProfileImageService.java
+++ src/main/java/d83t/bpmbackend/domain/aggregate/profile/service/ProfileImageService.java
@@ -2,8 +2,11 @@ package d83t.bpmbackend.domain.aggregate.profile.service;
 
 import d83t.bpmbackend.domain.aggregate.profile.dto.ProfileDto;
 import d83t.bpmbackend.domain.aggregate.profile.dto.ProfileRequest;
+import d83t.bpmbackend.domain.aggregate.profile.dto.ProfileUpdateRequest;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface ProfileImageService {
     ProfileDto createProfileDto(ProfileRequest profileRequest, MultipartFile file);
+
+    ProfileDto updateProfileDto(String fullPath, ProfileUpdateRequest profileUpdateRequest, MultipartFile file);
 }
