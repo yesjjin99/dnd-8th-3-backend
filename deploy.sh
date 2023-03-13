7b92089be8b583962fa4b69afafbfef89863b4c7
minseokkang <aaaa727@khu.ac.kr>
2023-03-14 02:11:05 +0900

    fix: nginx제외한 blue-green 배포 성공. 향후 nginx해결 필요

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
 
 # Blue 를 기준으로 현재 떠있는 컨테이너를 체크한다.
 EXIST_BLUE=$(sudo docker-compose -p web-blue -f docker-compose-blue.yml ps | grep Up)
+echo "EXIST_BLUE value is: $EXIST_BLUE"
 
-# 컨테이너 스위칭
 if [ -z "$EXIST_BLUE" ]; then
     echo "blue up"
     sudo docker-compose -p web-blue -f docker-compose-blue.yml up -d
@@ -22,8 +22,8 @@ sleep 10
 EXIST_AFTER=$(sudo docker-compose -p web-${AFTER_COMPOSE_COLOR} -f docker-compose-${AFTER_COMPOSE_COLOR}.yml ps | grep Up)
 if [ -n "$EXIST_AFTER" ]; then
     # nginx.config를 컨테이너에 맞게 변경해주고 reload 한다 -> 필요없을듯?
-    # cp /etc/nginx/nginx.${AFTER_COMPOSE_COLOR}.conf /etc/nginx/nginx.conf
-    nginx -s reload
+    sudo cp ./config/nginx/nginx.${AFTER_COMPOSE_COLOR}.conf /etc/nginx/nginx.conf
+    sudo nginx -s reload
     # 이전 컨테이너 종료
     sudo docker-compose -p web-${BEFORE_COMPOSE_COLOR} -f docker-compose-${BEFORE_COMPOSE_COLOR}.yml down
     echo "$BEFORE_COMPOSE_COLOR down"
