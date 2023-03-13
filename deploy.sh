5dd44614d9a6717e2f89151cd1a658342b288801
minseokkang <aaaa727@khu.ac.kr>
2023-03-14 00:16:34 +0900

    fix: docker compose service명 변경

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # Blue 를 기준으로 현재 떠있는 컨테이너를 체크한다.
-EXIST_BLUE=$(docker-compose -p web-blue -f docker-compose.blue.yml ps | grep Up)
+EXIST_BLUE=$(docker-compose -p web-blue -f docker-compose-blue.yml ps | grep Up)
 
 # 컨테이너 스위칭
 if [ -z "$EXIST_BLUE" ]; then
     echo "blue up"
-    docker-compose -p web-blue -f docker-compose.blue.yml up -d
+    docker-compose -p web-blue -f docker-compose-blue.yml up -d
     BEFORE_COMPOSE_COLOR="green"
     AFTER_COMPOSE_COLOR="blue"
 else
     echo "green up"
-    docker-compose -p web-green -f docker-compose.green.yml up -d
+    docker-compose -p web-green -f docker-compose-green.yml up -d
     BEFORE_COMPOSE_COLOR="blue"
     AFTER_COMPOSE_COLOR="green"
 fi
@@ -19,12 +19,12 @@ fi
 sleep 10
 
 # 새로운 컨테이너가 제대로 떴는지 확인
-EXIST_AFTER=$(docker-compose -p web-${AFTER_COMPOSE_COLOR} -f docker-compose.${AFTER_COMPOSE_COLOR}.yml ps | grep Up)
+EXIST_AFTER=$(docker-compose -p web-${AFTER_COMPOSE_COLOR} -f docker-compose-${AFTER_COMPOSE_COLOR}.yml ps | grep Up)
 if [ -n "$EXIST_AFTER" ]; then
     # nginx.config를 컨테이너에 맞게 변경해주고 reload 한다 -> 필요없을듯?
     #cp /etc/nginx/nginx.${AFTER_COMPOSE_COLOR}.conf /etc/nginx/nginx.conf
     nginx -s reload
     # 이전 컨테이너 종료
-    docker-compose -p web-${BEFORE_COMPOSE_COLOR} -f docker-compose.${BEFORE_COMPOSE_COLOR}.yml down
+    docker-compose -p web-${BEFORE_COMPOSE_COLOR} -f docker-compose-${BEFORE_COMPOSE_COLOR}.yml down
     echo "$BEFORE_COMPOSE_COLOR down"
 fi
