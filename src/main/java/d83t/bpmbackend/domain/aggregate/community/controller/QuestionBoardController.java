741e9172420c85cc4c53dc17a9b1b09c822302af
minseokkang <aaaa727@khu.ac.kr>
2023-04-14 22:18:03 +0900

    fix: 간단한 오타 수정

--- src/main/java/d83t/bpmbackend/domain/aggregate/community/controller/QuestionBoardController.java
+++ src/main/java/d83t/bpmbackend/domain/aggregate/community/controller/QuestionBoardController.java
@@ -74,7 +74,8 @@ public class QuestionBoardController {
             @PathVariable Long questionBoardArticleId) {
         log.info("data input: {}", questionBoardRequest.toString());
         return QuestionBoardResponse.SingleQuestionBoard.builder().questionBoardResponse(questionBoardService.updateQuestionBoardArticle(user, files, questionBoardRequest, questionBoardArticleId)).build();
-        
+    }
+
     @Operation(summary = "질문하기 게시판 게시글 삭제 API", description = "사용자가 질문하기 게시판 중 하나의 게시글을 클릭해서 삭제합니다. token을 넘겨야합니다.")
     @ApiResponse(responseCode = "200", description = "질문하기 게시판 게시글 삭제 성공")
     @ApiResponse(responseCode = "404", description = "게시글을 찾을 수 없습니다.", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
