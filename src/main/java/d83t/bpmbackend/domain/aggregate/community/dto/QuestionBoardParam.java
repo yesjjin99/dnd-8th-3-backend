8d740f6a84e90a19770443cc702d322eead487eb
kms <aaaa727@khu.ac.kr>
2023-04-23 15:24:50 +0900

    [BPM-105]: List 조회용 request DTO 추가

---
+++ src/main/java/d83t/bpmbackend/domain/aggregate/community/dto/QuestionBoardParam.java
@@ -0,0 +1,10 @@
+package d83t.bpmbackend.domain.aggregate.community.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class QuestionBoardParam {
+    private String nickname;
+}
